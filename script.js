ceb507711b90a9a32444a15a91f3dc125c7fa960
Rahulllkr <rahulkumar8604745@gmail.com>
2025-07-07 01:37:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const square1 = document.getElementById("square1")
+const square2 = document.getElementById("square2")
+const square3 = document.getElementById("square3")
+
+square1.addEventListener("mouseover", () => {
+	if(square1.style.background == "#E6E6FA"){
+		square2.style.background = "#6F4E37"
+		square3.style.background = "#6F4E37"
+	}
+})
+
+square2.addEventListener("mouseover", () => {
+	if(square2.style.background == "#E6E6FA"){
+		square1.style.background = "#6F4E37"
+		square3.style.background = "#6F4E37"
+	}
+})
+
+square3.addEventListener("mouseover", () => {
+	if(square3.style.background == "#E6E6FA"){
+		square1.style.background = "#6F4E37"
+		square2.style.background = "#6F4E37"
+	}
+})
