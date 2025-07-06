f715237cd3e038b07f0bb8b522e0650b390bf05e
Rahulllkr <rahulkumar8604745@gmail.com>
2025-07-07 01:45:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,23 +3,40 @@ const square1 = document.getElementById("square1")
 const square2 = document.getElementById("square2")
 const square3 = document.getElementById("square3")
 
+
+const lavender = "#E6E6FA"
+const coffee = "#6F4E37"
+
+function setSquareColor(s1,s2,s3) {
+	s1.style.background = "lavender"
+	s2.style.background = "coffee"
+	s3.style.background = "coffee"
+	
+}
+
 square1.addEventListener("mouseover", () => {
-	if(square1.style.background == "#E6E6FA"){
-		square2.style.background = "#6F4E37"
-		square3.style.background = "#6F4E37"
-	}
-})
+  setSquaresColor(square1, square2, square3);
+});
+square1.addEventListener("mouseout", () => {
+  resetColors();
+});
 
 square2.addEventListener("mouseover", () => {
-	if(square2.style.background == "#E6E6FA"){
-		square1.style.background = "#6F4E37"
-		square3.style.background = "#6F4E37"
-	}
-})
+  setSquaresColor(square2, square1, square3);
+});
+square2.addEventListener("mouseout", () => {
+  resetColors();
+});
 
 square3.addEventListener("mouseover", () => {
-	if(square3.style.background == "#E6E6FA"){
-		square1.style.background = "#6F4E37"
-		square2.style.background = "#6F4E37"
-	}
-})
+  setSquaresColor(square3, square1, square2);
+});
+square3.addEventListener("mouseout", () => {
+  resetColors();
+});
+
+function resetColors() {
+  square1.style.backgroundColor = lavender;
+  square2.style.backgroundColor = lavender;
+  square3.style.backgroundColor = lavender;
+}
